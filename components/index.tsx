2b02c69e3f6bf4076311464430777abdf2633b45
mimansha-swarup <mimanshaswarup@gmail.com>
2024-06-16 14:04:29 +0530

    skeleton3

--- components/index.tsx
+++ components/index.tsx
@@ -1,7 +1,7 @@
 import HomeSection from "./section/Home";
 import Blogs from "./section/blogs";
 import Contact from "./section/contact";
-import ProjectSection from "./section/ram";
+import ProjectSection from "./section/project";
 import Skills from "./section/skills";
 
 import Navbar from "./shared/Navbar";

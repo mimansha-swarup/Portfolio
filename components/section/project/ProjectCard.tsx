3cb76697c7cee84054aa796e5ec96e20784993d4
mimansha-swarup <mimanshaswarup@gmail.com>
2024-06-17 20:56:36 +0530

    project scroll animation

--- components/section/project/ProjectCard.tsx
+++ components/section/project/ProjectCard.tsx
@@ -23,30 +23,32 @@ const ProjectCard = ({ initial, animate }: IProjectCardProps) => {
   }, [ref?.current?.offsetWidth]);
 
   return (
-    <motion.div
-      // initial={{ y: initial }}
-      // animate={{ y: animate }}
-      // transition={{ duration: 1 }}
-      ref={ref}
-      className="relative w-full object-contain mx-auto   max-w-[75%] p-4 rounded-lg"
+    <div
       style={{
-        scale: scrollYProgress,
-        opacity: scrollYProgress,
-        background: "url(/assets/a.png)",
-        height,
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
         transform: `rotate(${generateRandomNumber()}deg)`,
       }}
     >
-      <div className=" absolute top-0 bottom-0 left-0 right-0 bg-dark-500 opacity-80" />
-      <h2 className="absolute p-0 pb-1 text-[1.75rem]  sm:text-[2rem]">
-        Title
-      </h2>
-      <p className="absolute bottom-0 pb-3 text-[.75rem] sm:text-[1rem] text-gray-500">
-        subTitle
-      </p>
-    </motion.div>
+      <motion.div
+        ref={ref}
+        className="relative w-full object-contain mx-auto   max-w-[75%] p-4 rounded-lg"
+        style={{
+          scale: scrollYProgress,
+          opacity: scrollYProgress,
+          background: "url(/assets/a.png)",
+          height,
+          backgroundSize: "cover",
+          backgroundPosition: "center center",
+        }}
+      >
+        <div className=" absolute top-0 bottom-0 left-0 right-0 bg-dark-500 opacity-80" />
+        <h2 className="absolute p-0 pb-1 text-[1.75rem]  sm:text-[2rem]">
+          Title
+        </h2>
+        <p className="absolute bottom-0 pb-3 text-[.75rem] sm:text-[1rem] text-gray-500">
+          subTitle
+        </p>
+      </motion.div>
+    </div>
   );
 };
 
--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,5 @@
 export const generateRandomNumber = () => {
-  const possibleValues = [-3, -2, -1, 1, 2, 3];
+  const possibleValues = [-5, -4, -3, -2, -1, 1, 2, 3, 4, 5];
   const randomIndex = Math.floor(Math.random() * possibleValues.length);
 
   return possibleValues[randomIndex];
